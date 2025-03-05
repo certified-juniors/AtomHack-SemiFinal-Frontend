f298c3ceb6f1534ed0291a1a4c6020c34cb12ac8
gramizor <granin17@yandex.ru>
2025-03-05 23:06:42 +0300

    api + notification + prettier + react-icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { FlowChart, Spaces } from "./pages"
-import { MantineProvider } from "@mantine/core"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { FlowChart, Spaces } from './pages'
+import { MantineProvider } from '@mantine/core'
 
 import '@mantine/core/styles.css'
+import '@mantine/notifications/styles.css'
+import { Notifications } from '@mantine/notifications'
 
 function App() {
 
   return (
     <MantineProvider>
+      <Notifications />
       <BrowserRouter>
         <Routes>
           <Route path="" element={<Spaces />} />
