698000c4941aa98de5b5b3412f74ee7c67f4566f
gramizor <granin17@yandex.ru>
2025-03-05 23:06:42 +0300

    api + notification + prettier + react-icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,38 @@ import { MantineProvider } from '@mantine/core';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { FlowChart, Spaces } from './pages';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { FlowChart, Spaces } from './pages'
+import { MantineProvider } from '@mantine/core'
 
 import '@mantine/core/styles.css';
+import '@mantine/core/styles.css'
+import '@mantine/notifications/styles.css'
+import { Notifications } from '@mantine/notifications'
 
 function App() {
-    return (
-        <MantineProvider>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="" element={<Spaces />} />
-                    <Route path="/space/:id" element={<FlowChart />} />
-                </Routes>
-            </BrowserRouter>
-        </MantineProvider>
-    );
+  return (
+    <MantineProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="" element={<Spaces />} />
+          <Route path="/space/:id" element={<FlowChart />} />
+        </Routes>
+      </BrowserRouter>
+    </MantineProvider>
+  );
+
+  return (
+    <MantineProvider>
+      <Notifications />
+      <BrowserRouter>
+        <Routes>
+          <Route path="" element={<Spaces />} />
+          <Route path="/space/:id" element={<FlowChart />} />
+        </Routes>
+      </BrowserRouter>
+    </MantineProvider>
+  )
 }
 
 export default App;
